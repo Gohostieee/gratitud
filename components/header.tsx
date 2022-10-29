0b51fafdbd52a632b58b6503b068b1e28068bd92
Gohoostieee <joshua32eb@gmail.com>
2022-10-29 16:14:49 -0400

    Updates to responsiveness

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@ import logo from "../ASSETS/logo gratitud.png"
 import Image from "next/image";
 import hamburger from "../ASSETS/hamburguer.png"
 import Link from "next/link";
+import {useState} from "react";
 
 interface data {
     url: string
@@ -29,16 +30,20 @@ const Header = ({url}: data) => {
         ["Blog", "/blog"],
         ["Contacto", "/contact"],
     ]
+    const [shadow, useShadow] = useState<boolean>(false)
 
     function Parse() {
         const array = []
         menuItems.forEach(x => {
             if (x[1] == url) {
-                array.push(<div className={"m-auto mb-4 mt-4"}><button className={" text-3xl underline text-accent border-primary cursor-default"}>{x[0]}</button></div>)
+                array.push(<div className={"m-auto mb-4 mt-4"}>
+                    <button className={" text-3xl underline text-accent cursor-default"}>{x[0]}</button>
+                </div>)
 
-            }
-            else{
-                array.push(<div className={"m-auto mb-4 mt-4"}><Link href={x[0]} ><button href = {x[1]} className={"softbtn text-3xl "}>{x[0]}</button></Link></div>)
+            } else {
+                array.push(<div className={"m-auto mb-4 mt-4"}><Link href={x[0]}>
+                    <button href={x[1]} className={"softbtn text-3xl "}>{x[0]}</button>
+                </Link></div>)
 
             }
 
@@ -49,24 +54,55 @@ const Header = ({url}: data) => {
 
     return (
         <>
-            <div className={"flex  h-[94px] shadow-lg lg:absolute fixed w-[100%] bg-neutral left-0"}>
+            <div className={"flex  h-[94px] shadow-lg lg:absolute fixed w-[100%] z-[150] bg-neutral left-0"}>
                 <div className={"lg:w-[20%] w-[100%] flex justify-end"}>
                     <Image alt={"logo"} className={"m-auto"} src={logo.src} width={300} height={90}/>
                     <div className="drawer lg:hidden inline-flx m-auto ">
                         <input id="my-drawer" type="checkbox" className="drawer-toggle"/>
                         <div className="drawer-content w-[100%] flex justify-end">
-                            <label htmlFor="my-drawer"
+                            <label onClick={function () {
+                                let tempShadow;
+                                switch (shadow) {
+                                    case false:
+                                        tempShadow = true
+                                        break;
+                                    case true:
+                                        tempShadow = false
+                                        break;
+
+                                }
+                                useShadow(tempShadow)
+                            }} htmlFor="my-drawer"
                                    className=" cursor-pointer h-[24px] mr-4 drawer-button mt-9 relative"><Image
                                 src={hamburger.src} width={32} height={32}/></label>
                         </div>
-                        <div className="drawer-side border-t top-[90px] fixed left-0 h-[100vh] z-[50]">
+                        <div className="drawer-side top-[94px] fixed left-0 h-[100vh] z-[50]">
                             <label htmlFor="my-drawer" className="drawer-overlay"></label>
-
                             <div className={"h-[100%] bg-neutral border-r-2 border-accent shadow-lg"}>
                                 <ul className="menu p-4 overflow-y-auto w-80 text-base-content">
                                     {Parse()}
+                                    <div className={"m-auto"}>
 
+                                        <Link href={"/signup"}>
+
+                                            <button
+                                                className={"hardbtn text-xl h-[44px] min-w-[144px] m-8 ml-8 mr-4 p-2 m-auto rounded-3xl"}>Crear
+                                                cuenta
+                                            </button>
+
+                                        </Link>
+                                    </div>
+                                    <div className={"m-auto"}>
+
+                                        <Link href={"/signin"}>
+
+                                            <button
+                                                className={"hardbtn text-xl h-[44px] min-w-[144px] m-8 ml-8 mr-4 p-2 m-auto rounded-3xl"}>Ingresar
+                                            </button>
+                                        </Link>
+                                    </div>
                                 </ul>
+
                             </div>
 
                         </div>
@@ -74,26 +110,42 @@ const Header = ({url}: data) => {
                 </div>
 
 
-                <div className={"lg:inline-flex hidden sm:w-[50%] w-[60%] flex justify-end"}>
+                <div className={"lg:inline-flex hidden sm:w-[50%] w-[60%] flex mt-4 justify-end"}>
                     {Parse()}
 
                 </div>
                 <div className={"lg:inline-flex hidden flex w-[30%] justify-center"}>
-                    <button
-                        className={"hardbtn text-xl h-[44px] min-w-[144px] m-8 ml-8 mr-4 p-2 m-auto rounded-3xl"}>Crear
-                        cuenta
-                    </button>
-                    <button
-                        className={"hardbtn text-xl h-[44px] min-w-[144px] m-8 ml-8 mr-4 p-2 m-auto rounded-3xl"}>Ingresar
-                    </button>
+                    <div>
+
+                        <Link href={"/signup"}>
+
+                            <button
+                                className={"hardbtn text-xl h-[44px] min-w-[144px] m-8 ml-8 mr-4 p-2 m-auto rounded-3xl"}>Crear
+                                cuenta
+                            </button>
+
+                        </Link>
+                    </div>
+                    <div>
+
+                        <Link href={"/signin"}>
+
+                            <button
+                                className={"hardbtn text-xl h-[44px] min-w-[144px] m-8 ml-8 mr-4 p-2 m-auto rounded-3xl"}>Ingresar
+                            </button>
+                        </Link>
+                    </div>
 
                 </div>
+
                 <div className={""}>
 
 
                 </div>
             </div>
-
+            {
+                shadow && <div className={"fixed left-0 top-0 w-[100vw] h-[100vh] z-[100] bg-accent bg-opacity-40"}/>
+            }
         </>
     )
 }
