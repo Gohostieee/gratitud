8650558b915b5e99e6c044c1a9ceabf24848ef3d
Cdogo <john.tanaristy@gmail.com>
2022-11-13 19:58:54 -0600

    Added terms of service and privacy page

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function TermsOfService(){
     return(<main className={"flex flex-col m-auto"}>
         <div className={"lg:w-[1300px] relative top-[128px] m-auto h-screen"}>
-            <h1 className={"text-primary lg:text-4xl text-3xl text-center lg:ml-24 font-bold "}>Terms of service</h1>
+            <h1 className={"text-primary lg:text-4xl text-3xl text-center lg:ml-24 font-bold "}>Adviso de privacidad</h1>
             <p className={"text-primary lg:text-2xl lg:mt-8 text-lg text-center lg:ml-24 font-mono "}>Lorem ipsum sit ammet lormeLorem ipsum sit ammet lormeLorem ipsum sit ammet lormeLorem ipsum sit ammet lormeLorem ipsum sit ammet lormeLorem ipsum sit ammet lormeLorem ipsum sit ammet lormeLorem ipsum sit ammet lormeLorem ipsum sit ammet lormeLorem ipsum sit ammet lormeLorem ipsum sit ammet lorme</p>
         </div>
     </main>)
